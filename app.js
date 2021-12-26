3c3e9cf6674b3b1b90f097c429046d629e91d631
allenlin90 <ht016746@gmail.com>
2021-12-26 18:19:13 +0700

    add '/gecko/coins'

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const CoinGecko = require('coingecko-api');
+const CoinGeckoClient = new CoinGecko();
+
+app.get('/', (req, res) => {
+  res.send(`server is working at ${new Date()}`);
+});
+
+app.get('/gecko/coins', async (req, res) => {
+  // let data = await CoinGeckoClient.coins.all();
+  let data = await CoinGeckoClient.coins.markets();
+  res.json(data);
+});
+
+const PORT = 3000;
+app.listen(PORT, (err) => {
+  if (err) console.log(err);
+  console.log(`server starts on port ${PORT}`);
+});
